1d0b6e9343477e07f0a094d27e6eb64fc333fbd9
Shenba-Ps <101085607+Shenba-Ps@users.noreply.github.com>
2023-06-15 13:15:36 +0800

    Create Booking.js

---
+++ Booking.js
@@ -0,0 +1,11 @@
+$(function() {
+	
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+	OpenCustomerDetail();
+});
+
+function OpenCustomerDetail() {
+	$('#mdlCustomerDetail').modal('show');
+}
